33680e63ca4205e231e2a74b7ac7a1e711b8d7be
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2022-04-03 10:07:10 +0800

    :sparkles: start ice server

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
+#include <Ice/Ice.h>
+#include <Server.h>
+#include <string>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+using namespace std;
+using namespace Server;
+
+class SerialPortI : public SerialPort
+{
+public:
+    virtual ::std::string readSerialPort(const ::Ice::Current& = ::Ice::Current());
+};
+
+::std::string SerialPortI::readSerialPort(const ::Ice::Current&)
+{
+    return "Hello world!";
+}
+
+int main(int argc, char* argv[]) {
+    int status = 0;
+    Ice::CommunicatorPtr ic;
+    try {
+        ic = Ice::initialize(argc, argv);
+        Ice::ObjectAdapterPtr adapter
+                = ic->createObjectAdapterWithEndpoints(
+                        "ServerAdapter", "default -p 10000");
+        Ice::ObjectPtr object = new SerialPortI();
+        adapter->add(object, ic->stringToIdentity("SerialPort")
+        );
+        adapter->activate();
+        ic->waitForShutdown();
+    } catch (const Ice::Exception &e) {
+        cerr << e << endl;
+        status = 1;
+    } catch (const char* msg) {
+        cerr << msg << endl;
+        status = 1;
+    }
+    if (ic) {
+        try {
+            ic->destroy();
+        } catch (const Ice::Exception& e) {
+            cerr << e << endl;
+            status = 1;
+        }
+    }
+    return status;
 }
